8f1520917f31722cb152f8f1267c6499bae529e2
Toxic Waste <sarmatkociev44@gmail.com>
2025-04-11 00:27:24 +0300

    Task1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,29 +3,42 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    private final UserDao userDao = new UserDaoJDBCImpl();
+    public void createUsersTable() {
+        userDao.createUsersTable();
+        System.out.println("Таблица Users создана.");
     }
 
     public void dropUsersTable() {
+        userDao.dropUsersTable();
+        System.out.println("Таблица Users уничтожена.");
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        userDao.saveUser(name, lastName, age);
+        System.out.printf("Пользователь %s %s %d был добавлен.\n", name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
+        userDao.removeUserById(id);
+        System.out.print("Пользователь id = " + id + " успешно удален.\n");
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
+        userDao.cleanUsersTable();
+        System.out.println("Таблица Users очищена.");
 
     }
 }
